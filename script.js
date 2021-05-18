a80ccfbc4b14cda5c10945681129b991b91eb96f
Matthew Lopez <mlopez@domain.local>
2021-05-18 17:49:32 -0400

    used jquery for time--much easier

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-function toDate() {
 
-    var newDate = moment()
-
-    var displayDate = document.getElementById('currentDay');
-    displayDate.innerHTML = moment().format("[Today's Date is: ] dddd, MMM Do");
-
-
-
-};
-
-toDate();
+$("#currentDay").text(moment().format("[Today's Date is: ] dddd, MMM Do")); //easier way to write time in JQuery using moment
