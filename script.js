4a612f2927a3928dc432817bf4bc2487006c7864
mlopez <lopezmatthew87@gmail.com>
2021-05-19 01:03:43 -0400

    working on .timeblock class

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 $("#currentDay").text(moment().format("[Today's Date is: ] dddd, MMM Do")); //easier way to write time in JQuery using moment
 
+var currentTime = parseInt(moment().format("HH")); // turning hour into an integer to test number
+
+$(".time-block").each(function () {
+  var hrDiv = parseInt($(this).attr("id").split(".")[1]);
+
+  if (hrDiv < currentTime) {
+    $(this).find("input").addClass("future");
+
+  } else if (hrDiv == currentTime) {
+      $(this).find("input").addClass("present");
+
+  } else (hrDiv > currentTime) {
+      $(this).find("input").addClass("past");
+  }
+
+//   var tmID = 
+
+//   va
+
+
+
+
+});
+
+$("saveBtn").click(function() {
+
+})
